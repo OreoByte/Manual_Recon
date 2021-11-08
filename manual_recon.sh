0c16be2939d4ab884ec90bf47847bf509b69faa6
IO-Shield <isaacoren42@gmail.com>
2021-11-08 14:28:28 -0800

    updated pivoting

--- manual_recon.sh
+++ manual_recon.sh
@@ -3,7 +3,10 @@ RHOST='127.0.0.1'
 RPORT='4444'
 LHOST='127.0.0.1'
 LPORT='6900'
+SUBNET='127.0.0.1/24'
+INTERALHOST='172.16.10.5'
 USER='UserName'
+LUSER='kali'
 PW='P@ssw0rd'
 DB='Database_Name'
 Domain='domain.local'
@@ -16,9 +19,11 @@ case "$1" in
 	RHOST=$(cat $config|grep RHOST|grep -o -P "(?<=').*(?=')")
 	RPORT=$(cat $config|grep RPORT|grep -o -P "(?<=').*(?=')")
 	LHOST=$(cat $config|grep LHOST|grep -o -P "(?<=').*(?=')")
+	SUBNET=$(cat $config|grep SUBNET|grep -o -P "(?<=').*(?=')")
 	DB=$(cat $config|grep DB|grep -o -P "(?<=').*(?=')")
 	Domain=$(cat $config|grep Domain|grep -o -P "(?<=').*(?=')")
 	USER=$(cat $config|grep USER|grep -o -P "(?<=').*(?=')")
+	LUSER=$(cat $config|grep 'LUSER'|grep -o -P "(?<=').*(?=')")
 	PW=$(cat $config|grep PW|grep -o -P "(?<=').*(?=')")
 	GROUP=$(cat $config|grep GROUP|grep -o -P "(?<=').*(?=')");;
 	-rh) RHOST=$2;;
@@ -30,6 +35,9 @@ case "$1" in
 	-u) USER=$2;;
 	-p) PW=$2;;
 	-g) GROUP=$2;;
+	-lu) LUSER=$2;;
+	-ih) INTERALHOST=$2;;
+	-s) SUBNET=$2
 esac
 shift
 done
@@ -53,15 +61,18 @@ man_help() {
 	echo -e "# RHORT, RPOST, LHOST, LPORT, and other User Arguments\n "
 	echo -e "-rh <RHOST-IP> | RHOST/Target IP-Address\n-rp <RPORT-NUM> | RPORT or Port Number Of Target"
 	echo -e "-lp <LHOST-IP> | LHOST/Listen IP-Address\n-lp <LPORT-NUM> | LPORT or Port Number Of Listener\n"
+	echo -e "-s <SUBNET> | <SUBNET/SUBNET_MASK> To set for certain cheat sheets like network pivoting\n"
 	echo -e "# Domain Nane and Database Name\n\n-db <DB_Name> | Database Name To Use or Select"
 	echo -e "-d <Domain_Name> | Domain or Netbios Name Of Remote RHOST\n"
 	echo -e "# Custom Username and Password\n\n-u <username> | Username Of A Discovered User\n-p <password> | Password To Test or Has Been Discovered\n"
 	echo -e "# Additional Arguments For Some Cheat Sheats\n\n-g <group-name> | Set a custom group name for some options"
+	echo -e "-lu <local-username> | Set a username for your own or local machnie used in Pivoting and cheat sheets"
 	echo -e "\n{!} NOTE: Required User Arguments May Change Based The Option Used {!}\n"
 	echo "#--------------------------------------------------------------------------------"
 	echo -e "# Manual Recon Examples Commands\n"
-	echo -e "./manual_recon.sh -o w -rh 10.10.10.1\n./manual_recon.sh -c user.config -u superadmin\n"
-	echo "#--------------------------------------------------------------------------------------"
+	echo -e "./manual_recon.sh -o w -rh 10.10.10.1\n./manual_recon.sh -c user.config -u superadmin"
+	echo -e "./manual_recon.sh -o n -rh 10.10.138.2 -s 10.10.138.0/24 -u cthulhu -lh 10.15.28.40 -ih 172.16.4.21"
+	echo -e "\n#--------------------------------------------------------------------------------------"
 }
 print_all_manual_recon(){
         . ./recon_scripts/windows
